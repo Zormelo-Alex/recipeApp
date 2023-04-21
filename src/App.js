9e9b50909c153a1d0242e5e595b03604772e9347
Zormelo_Alex <alexzormelo9@gmail.com>
2023-04-21 20:14:01 +0000

    finished the first page

--- src/App.js
+++ src/App.js
@@ -1,48 +1,88 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Loading from "./components/Loading";
 
 function App() {
   //console.log("Dom")
-  const category = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
   const [data, setdata] = useState(null);
+  const [search, setsearch] = useState("");
 
-const getCategory = () => {
-    const cards = document.querySelector(".cards")
-    fetch(category)
-    .then(res=>res.json())
-    .then(data=>{
-      setdata(data);
-      //console.log(data);
-    })
-    .catch(err=>{
-        console.log(err)
+  const categories = "https://www.themealdb.com/api/json/v1/1/categories.php";
+  const searchAPI = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+
+
+  const getCategory = () => {
+    const cards = document.querySelector(".cards");
+
+    //getting all food categories
+    fetch(categories)
+      .then((res) => res.json())
+      .then((data) => {
+        setdata(data);
+        console.log(data.categories);
+      })
+      .catch((err) => {
+        console.log(err);
         cards.textContent = err.message;
-        setInterval(getCategory, 1000)
-    })
-}
+        setInterval(getCategory, 1000);
+      });
+  };
 
-useEffect(getCategory,[]);
+  
+  useEffect(()=>{
+    return()=>{
+      // toast.success("Welcome", {
+      //   position: "top-left",
+      //   })
+      getCategory()
+    }
+  }
+    , []);
 
   return (
     <div className="App">
-      <h3 className="head">Food Categories</h3>
-      <div className="categories">
-        <div className="cards">
-          {data? data.categories.map(info=>{
-            <div className='card' key={info.idCategory}>
-              <a href="#">
-                <div className="image">
-                  <img src={info.strCategoryThumb} alt="" />
-                </div>
-                <div className="name">{info.strCategory}</div>
-                <div className="desc">{info.strCategoryDescription}</div>
-              </a>
-            </div>
-            //console.log(info)
-          })
-            
-          : <p>Loading...</p>}
+      <ToastContainer />
+      <div className="top">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search meal..."
+          />
+          <AiOutlineSearch className="cursor-pointer w-8 pl-2 pr-2 h-full" 
+          />
+        </div>
+        <div>
+          <h2 className="text-white text-5xl font-bold">Explore Recipes</h2>
+          <p>Food is a vital part of our daily lives, providing us with the energy and nutrients we need to sustain ourselves. From fast food to gourmet cuisine, there is a vast array of food options available to us, each with its own unique flavor and cultural significance. Food can be a source of comfort, a way to bring people together, or a means of expressing creativity. However, it's important to remember that not all foods are created equal, and a balanced diet is key to maintaining good health.</p>
+        </div>
+      </div>
+      <div className="list">
+        <h3 className="header">Food Categories</h3>
+        <h5>You have {data? <span>{data.categories.length}</span> : <span>-</span>} categories to explore</h5>
+        <div>
+          <div className="categories-container">
+            {data ? (
+              data.categories.map((info) => {
+                return (
+                  <div className="category-container" key={info.idCategory}>
+                    <a className="category-content" href="#">
+                      <div className="image">
+                        <img src={info.strCategoryThumb} alt="" />
+                      </div>
+                      <div className="name">{info.strCategory}</div>
+                      <div className="desc">{info.strCategoryDescription}</div>
+                    </a>
+                  </div>
+                );
+              })
+            ) : (
+              <Loading />
+            )}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Loading.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import '../stylesheets/loading.css'
+
+const Loading = () => {
+  return (
+    <div>
+        <div className="loading-card">
+            <div className="loading-top">
+                <div className="img skeleton"></div>
+                <div className="w-full">
+                    <div className='skeleton skeleton-text'></div>
+                    <div className='skeleton skeleton-text'></div>
+                </div>
+            </div>
+            <div className="loading-bottom">
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+            </div>
+        </div>
+        <div className="loading-card">
+            <div className="loading-top">
+                <div className="img skeleton"></div>
+                <div className="w-full">
+                    <div className='skeleton skeleton-text'></div>
+                    <div className='skeleton skeleton-text'></div>
+                </div>
+            </div>
+            <div className="loading-bottom">
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+            </div>
+        </div>
+        <div className="loading-card">
+            <div className="loading-top">
+                <div className="img skeleton"></div>
+                <div className="w-full">
+                    <div className='skeleton skeleton-text'></div>
+                    <div className='skeleton skeleton-text'></div>
+                </div>
+            </div>
+            <div className="loading-bottom">
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+                <div className='skeleton skeleton-text'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Loading;
